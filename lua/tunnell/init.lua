40186009b34c8fc75a87582fd72457afc7254d2e
Mateus Rodolfo <mateus.rodolfo.1998@gmail.com>
2023-07-18 20:51:59 +0200

    better command handling

--- lua/tunnell/init.lua
+++ lua/tunnell/init.lua
@@ -7,7 +7,7 @@ local defaults = {
 }
 
 -- Sets buffer-variables `cell_header` and `tmux_target` to values given by user via `vim.fn.input`
-function M.config()
+local function config()
     -- cell_header
     vim.b.cell_header = vim.fn.input({
         prompt = "Cell header: ",
@@ -23,25 +23,25 @@ function M.config()
     })
 end
 
--- Sends active selection to target
+-- Tunnells active selection to target
 --
--- Does not accept a range, so <C-U> to remove `'<,'>`
-function M.send_selection()
-    -- load buffer with lines from active selection
-    vim.cmd("silent '<,'>:w !tmux load-buffer - ")
+-- Reads `args.line1` and `args.line2` from a range
+local function tunnell_range(r)
+    -- load buffer with range from `args.line1` to `args.line2`
+    vim.cmd("silent " .. r.line1 .. "," .. r.line2 .. ":w !tmux load-buffer - ")
 
-    -- send lines
+    -- tunnell lines
     local target = vim.b.tmux_target and vim.b.tmux_target or defaults.tmux_target
     vim.fn.system("tmux paste-buffer -dpr -t " .. target)
 
-    -- send <CR> to run cell in REPL
+    -- tunnell <CR> to run cell in REPL
     vim.fn.system("tmux send-keys -t " .. target .. " Enter")
 end
 
--- Sends cell to target
+-- Tunnells cell to target
 --
 -- Cursor does not have to be on the cell header, but anywhere inside the cell
-function M.send_cell()
+local function tunnell_cell()
     -- load cell_header
     local cell_header = vim.b.cell_header and vim.b.cell_header or defaults.cell_header
 
@@ -54,25 +54,18 @@ function M.send_cell()
         return
     end
 
-    -- define cell end
+    -- define end of cell
     local end_line = vim.fn.search(cell_header, "nW")
 
     -- if no header found below cursor, cursor is in the last cell so end line should be the
     -- last line of the file. Otherwise, end line is one line above next cell header
     end_line = end_line == 0 and vim.fn.line("w$") or end_line - 1
 
-    -- load buffer with lines
-    vim.cmd("silent " .. start_line .. "," .. end_line .. ":w !tmux load-buffer - ")
-
-    -- send lines
-    local target = vim.b.tmux_target and vim.b.tmux_target or defaults.tmux_target
-    vim.cmd("silent !tmux paste-buffer -dpr -t " .. target)
-
-    -- send <CR>
-    vim.cmd("silent !tmux send-keys -t " .. target .. " BSpace Enter")
+    -- tunnell cell range
+    tunnell_range({ line1 = start_line, line2 = end_line })
 
     -- put curson on next cell
-    vim.cmd("silent /" .. cell_header)
+    vim.cmd("/" .. cell_header)
 end
 
 -- Setup function for users to call from their plugin managers
@@ -81,14 +74,14 @@ function M.setup(user_config)
     defaults = vim.tbl_deep_extend("force", defaults, user_config or {})
 
     -- set commands
-    vim.api.nvim_create_user_command("TunnelConfig", M.config, {})
-    vim.api.nvim_create_user_command("TunnellSelection", M.send_selection, {})
-    vim.api.nvim_create_user_command("TunnellCell", M.send_cell, {})
+    vim.api.nvim_create_user_command("TunnelConfig", config, {})
+    vim.api.nvim_create_user_command("TunnellSelection", tunnell_range, { range = true })
+    vim.api.nvim_create_user_command("TunnellCell", tunnell_cell, {})
 
     -- keymaps if desired by user
     if defaults.use_default_keymaps then
         vim.keymap.set({ "n" }, "<leader>t", ":TunnellCell<CR>", { desc = "Tunnell cell" })
-        vim.keymap.set({ "v" }, "<leader>t", ":<C-U>TunnellSelection<CR>", { desc = "Tunnell selection" })
+        vim.keymap.set({ "v" }, "<leader>t", ":TunnellSelection<CR>", { desc = "Tunnell selection" })
     end
 end
 
